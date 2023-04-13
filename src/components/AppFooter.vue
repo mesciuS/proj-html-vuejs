507ff26d2d77bcd192ae1205d88ad626ce9042f8
mesciuS <giovadev@gmail.com>
2023-04-13 14:34:19 +0200

    feat: footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,11 +9,54 @@ export default {
 </script>
 
 <template>
-    <div>
-
+    <div id="container">
+      <div class="top">
+        <img src="/images/logo-small.png" alt="">
+      </div>
+      <div class="bottom">
+        <p>&copy; Copyright 2012-2022 | Avada Theme by <span>ThemeFusion</span> | All Rights Reserved | Powered by <span>ME</span></p>
+        <div class="loghi">
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-
+  #container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    height: 200px;
+    .top {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 55%;
+    }
+    .bottom {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      width: 100%;
+      height: 45%;
+      background-color: #f9f9f9;
+      span {
+        color: #fdd182;
+      }
+      .loghi {
+        display: flex;
+        gap: 10px;
+        i {
+          font-size: 2em;
+          color: #fdd182;
+        }
+      }
+    }
+  }
 </style>
